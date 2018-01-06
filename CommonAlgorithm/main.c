3ae05b61932e8089ab5ef752db49683ed9ff1177
wangjia1435@shu.edu.cn <wangjia1435@shu.edu.cn>
2018-01-06 16:06:32 +0800

    Initial Commit

---
+++ CommonAlgorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CommonAlgorithm
+//
+//  Created by Jia Wang on 2018/1/6.
+//  Copyright © 2018年 Jia Wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
